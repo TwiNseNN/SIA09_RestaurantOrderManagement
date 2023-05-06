2c6bb160ad3c3e43c5b06c88bb3cd68d388a3b3b
TwiNseNN <heghismaricel@yahoo.com>
2023-05-07 00:16:32 +0300

    Update 7/05/2023

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagementOrder.Models.Statistics;
+using Stripe;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementOrder.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        protected Data.RestaurantManagementOrderContext _context;
+        public StatisticsController(Data.RestaurantManagementOrderContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(new StatisticsModel
+            {
+                BiggestOrderByQuantity = await _context.Order
+                    .Include(o => o.OrderItems)
+                    .ThenInclude(o => o.MenuItem)
+                    .OrderByDescending(o => o.OrderItems.Sum(oi => oi.Quantity))
+                    .FirstOrDefaultAsync(),
+                BiggestOrderByPrice = _context.Order
+                    .Include(o => o.OrderItems)
+                    .ThenInclude(o => o.MenuItem)
+                    .ToList()
+                    .OrderByDescending(o => o.Total)
+                    .FirstOrDefault(),
+                MostExpensiveTable = _context.Order
+                    .Include(o => o.OrderItems)
+                    .ThenInclude(o => o.MenuItem)
+                    .GroupBy(o => o.TableNumber)
+                    .ToList()
+                    .Select(gr => new
+                    {
+                        tableNum = gr.Key,
+                        totalPrice = gr.Sum(o => o.Total)
+                    })
+                    .OrderByDescending(gr => gr.totalPrice)
+                    .Select(gr => new MostExpensiveTableInfo
+                    {
+                        TableNum = gr.tableNum, TotalPrice = gr.totalPrice,
+                    })
+                    .FirstOrDefault(),
+            });
+        }
+    }
+}
---
+++ Models/Statistics/StatisticsModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RestaurantManagementOrder.Models.Statistics
+{
+    public class MostExpensiveTableInfo
+    {
+        public int TableNum { get; set; }
+        public int TotalPrice { get; set; }
+    }
+
+    public class StatisticsModel
+    {
+        public Order BiggestOrderByQuantity { get; set; }
+        public Order BiggestOrderByPrice { get; set; }
+        // (table #, total price)
+        public MostExpensiveTableInfo MostExpensiveTable { get; set; }
+    }
+}
